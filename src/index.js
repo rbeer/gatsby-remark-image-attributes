08bd3b7eb1fe9afd9d7e7a4c6eb32eba1b3f25b5
Raphael Beer <raphael.beer@protonmail.com>
2020-04-02 23:11:30 +0200

    Clean: lint, prettier

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
-const visit = require("unist-util-visit");
-const isArray = require("lodash.isarray");
-const isString = require("lodash.isstring");
-const uniq = require("lodash.uniq");
-const $ = require("cheerio");
+const visit = require('unist-util-visit');
+const isArray = require('lodash.isarray');
+const isString = require('lodash.isstring');
+const uniq = require('lodash.uniq');
+const $ = require('cheerio');
 
 let styleAttributes = [
-  "width",
-  "height",
-  "margin-left",
-  "margin-right",
-  "margin-top",
-  "margin-bottom",
-  "float"
+  'width',
+  'height',
+  'margin-left',
+  'margin-right',
+  'margin-top',
+  'margin-bottom',
+  'float'
 ];
 
 const applyOptions = ({ styleAttributes: _styleAttributes }, reporter) => {
   if (_styleAttributes) {
     if (!isArray(_styleAttributes)) {
-      reporter.warn("Option styleAttributes must be an Array of strings.");
+      reporter.warn('Option styleAttributes must be an Array of strings.');
     }
     styleAttributes = uniq(
       styleAttributes.concat(_styleAttributes.filter(isString))
@@ -25,7 +25,7 @@ const applyOptions = ({ styleAttributes: _styleAttributes }, reporter) => {
   }
 };
 
-const isGatsbyRemarkImagesHtml = node => {
+const isGatsbyRemarkImagesHtml = (node) => {
   const hasAttributes = !!node.attributes;
   const hasUrl = !!node.url;
   const isImage = /<img/.test(node.value);
@@ -33,20 +33,20 @@ const isGatsbyRemarkImagesHtml = node => {
   return hasAttributes && hasUrl && isImage;
 };
 
-const createStyle = attributes => {
+const createStyle = (attributes) => {
   const attributeKeys = Object.keys(attributes);
   return attributeKeys.reduce((styles, attribute) => {
     if (!styleAttributes.includes(attribute)) {
       return styles;
     }
     return `${styles}${attribute}: ${attributes[attribute]};`;
-  }, "");
+  }, '');
 };
 
 const createImgMarkup = ({ attributes, url, alt }) => `
   <img
     src="${url}"
-    style="${createStyle(attributes) || "width: 100%;"}"
+    style="${createStyle(attributes) || 'width: 100%;'}"
     alt="${alt}"
   />
   `;
@@ -55,7 +55,7 @@ const amendImgMarkup = ({ attributes, value }) => {
   const $fullMarkup = $.load(`<span style="display:block">${value}</span>`, {
     xmlMode: true
   });
-  $fullMarkup("img").attr({ style: createStyle(attributes) });
+  $fullMarkup('img').attr({ style: createStyle(attributes) });
   console.log($fullMarkup.html());
   return $fullMarkup.html();
 };
@@ -64,31 +64,31 @@ module.exports = ({ markdownAST, reporter }, options) => {
   applyOptions(options, reporter);
 
   const gatsbyRemarkImagesHtml = [];
-  visit(markdownAST, "html", node => {
+  visit(markdownAST, 'html', (node) => {
     if (isGatsbyRemarkImagesHtml(node)) gatsbyRemarkImagesHtml.push(node);
   });
 
   const images = [];
-  visit(markdownAST, "image", node => {
+  visit(markdownAST, 'image', (node) => {
     // only process images with attributes
-    if (!!node.attributes) {
+    if (node.attributes) {
       images.push(node);
     }
   });
 
   return Promise.all([
-    new Promise(resolve =>
+    new Promise((resolve) =>
       resolve(
-        images.map(imageNode => {
-          imageNode.type = "html";
+        images.map((imageNode) => {
+          imageNode.type = 'html';
           imageNode.value = createImgMarkup(imageNode);
           return imageNode;
         })
       )
     ),
-    new Promise(resolve =>
+    new Promise((resolve) =>
       resolve(
-        gatsbyRemarkImagesHtml.map(imgNode => {
+        gatsbyRemarkImagesHtml.map((imgNode) => {
           const amended = amendImgMarkup(imgNode);
           imgNode.value = amended;
           return imgNode;
