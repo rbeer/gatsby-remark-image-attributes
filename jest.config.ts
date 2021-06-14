ad0a1a651eb8e084646cf4c699891ebdfd76e444
Raphael Beer <raphael.beer@protonmail.com>
2021-06-14 11:58:26 +0200

    Add: WrappedAttributeImage tests and coverage +
      Adds 2 npm scripts
        - cov
          Runs tests with --coverage
          Output to /coverage
    
        - covdev
          Runs tests with --watch, --coverage

--- jest.config.ts
+++ jest.config.ts
@@ -3,14 +3,14 @@ const useCoverage = !!process.env.GENERATE_JEST_REPORT;
 export default {
   notify: true,
   verbose: true,
-  roots: ['./src/__tests__'],
+  testPathIgnorePatterns: ['./dist/__tests__'],
   snapshotSerializers: ['jest-serializer-path'],
-  collectCoverageFrom: ['./src/**/*.ts'],
+  collectCoverageFrom: ['./src/**/*.ts', '!./src/__tests__/'],
   reporters: process.env.CI
     ? [['jest-silent-reporter', { useDots: true }]].concat(
         useCoverage ? ['jest-junit'] : []
       )
     : ['default'].concat(useCoverage ? ['jest-junit'] : []),
-  transform: { '\\.[jt]sx?$': 'ts-jest' },
+  transform: { '\\.[t]sx?$': 'ts-jest' },
   moduleFileExtensions: ['js', 'ts', 'json']
 };
